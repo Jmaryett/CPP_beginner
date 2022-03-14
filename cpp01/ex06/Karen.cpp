78f52ddcc6681d2fd6f75dcf8f52f5625f04e0ee
jmaryett <wtfmahariel@gmail.com>
2022-03-14 18:51:39 +0300

    cpp01

---
+++ cpp01/ex06/Karen.cpp
@@ -0,0 +1,52 @@
+#include "Karen.hpp"
+
+Karen::Karen()
+{
+}
+
+Karen::~Karen()
+{
+}
+
+void	Karen::debug(void)
+{
+	std::cout << "I love having extra bacon for" ;
+	std::cout << "my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Karen::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put ";
+	std::cout << "enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void	Karen::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for ";
+	std::cout << "years whereas you started working here since last month.\n";
+}
+
+void	Karen::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Karen::complain(std::string level)
+{
+	std::string	phrases[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void	(Karen::*funcPtr[4])() = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	for (int i = 0; i < 4; i++)
+	{
+		if (phrases[i] == level)
+		{
+			switch (i) {
+				case 0: (this->*funcPtr[0])();
+				case 1: (this->*funcPtr[1])();
+				case 2: (this->*funcPtr[2])();
+				case 3: (this->*funcPtr[3])();
+			}
+			return ;
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -2,5 +2,14 @@
 
 int	main(int ac, char **av)
 {
-	
+	Karen	karen;
+
+	if (ac != 2)
+	{
+		std::cout << "Wrong number of args!\n";
+		return (0);
+	}
+	else
+		karen.complain(static_cast<std::string>(av[1]));
+	return (0);
 }
