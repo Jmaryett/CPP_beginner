7cd9d42db7c23356b659c19d0316322d7842185b
jmaryett <wtfmahariel@gmail.com>
2022-04-01 17:26:24 +0300

    for you my loveeeeeeeeeeee

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <limits.h>
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{ std::cout << "Wrong number of args!\n"; return (1); }
+
+
+}
